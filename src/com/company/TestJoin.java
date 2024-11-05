18ef7d2b533c38bc97b06c7903d60651a9289fe7
hongkai40659 <hongkai40659@hundsun.com>
2024-11-05 14:52:03 +0800

    join的功能

---
+++ src/com/company/TestJoin.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class TestJoin {
+
+    /**
+     * 1、join的用途【插队】
+     * 2、线程按照一定的顺序执行.
+     */
+
+    private static class Gril implements Runnable{
+        private Thread th;
+        @Override
+        public void run() {
+            if (th !=null) {
+                try {
+                    th.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            SleepTools.second(1); // 模拟业务运行
+            System.out.println(Thread.currentThread().getName()+" finished");
+        }
+    }
+
+    private static class GrilFriend implements Runnable{
+
+        @Override
+        public void run() {
+            SleepTools.second(1);  // 模拟业务运行
+            System.out.println(Thread.currentThread().getName()+" finished");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread girl = new Thread(new Gril());
+        girl.setName("girl");
+
+        Thread girlFriend = new Thread(new GrilFriend());
+        girlFriend.setName("girlFriend");
+
+        girl.start();
+        girlFriend.start();
+
+        girl.join();
+
+        System.out.println(Thread.currentThread().getName()+" finished");
+
+
+
+    }
+
+
+}
